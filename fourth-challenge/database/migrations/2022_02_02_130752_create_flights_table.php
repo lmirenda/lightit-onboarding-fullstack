3cd5920e571b3fc9bdfca229db82194b57e917c1
lmirenda <lucamateomirenda@gmail.com>
2022-02-01 12:40:05 -0300

    Add tailwind support. Config database migrations and seeders.

---
+++ fourth-challenge/database/migrations/2022_02_02_130752_create_flights_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('origin_city_id')->references('id')->on('cities');
+            $table->foreignId('destination_city_id')->references('id')->on('cities');
+            $table->foreignId('company_id')->constrained();
+            $table->dateTime('departure');
+            $table->dateTime('arrival');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
