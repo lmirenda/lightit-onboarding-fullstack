258f9aa0c110994b766865e45af55a009a9cb551
lmirenda <lucamateomirenda@gmail.com>
2022-02-21 14:56:29 -0300

    Clean up flight controller update test

--- fourth-challenge/tests/Feature/Flight/FlightUpdateTest.php
+++ fourth-challenge/tests/Feature/Flight/FlightUpdateTest.php
@@ -18,35 +18,28 @@ class FlightUpdateTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $oldCompany = Company::factory()->create();
-        $oldOrigin = City::factory()->create();
-        $oldDestination = City::factory()->create();
-
         $newCompany = Company::factory()->create();
         $newOrigin = City::factory()->create();
         $newDestination = City::factory()->create();
+        $newTime = time();
 
-        $flight = Flight::factory()->create([
-            'origin_city_id' => $oldOrigin['id'],
-            'destination_city_id' => $oldDestination['id'],
-            'company_id' => $oldCompany['id'],
-            'departure' => time(),
-            'arrival' => time()
-        ]);
+        $flight = Flight::factory()->create();
 
         $this
             ->put(action([FlightController::class, 'update'],$flight->id),[
                 'origin_city_id' => $newOrigin['id'],
                 'destination_city_id' => $newDestination['id'],
                 'company_id' => $newCompany['id'],
-                'departure' => time(),
-                'arrival' => time()
+                'departure' => $newTime,
+                'arrival' => $newTime
             ]);
 
         $this->assertDatabaseHas(Flight::class,[
             'origin_city_id' => $newOrigin['id'],
             'destination_city_id' => $newDestination['id'],
-            'company_id' => $newCompany['id']
+            'company_id' => $newCompany['id'],
+            'departure' => $newTime,
+            'arrival' => $newTime
         ]);
     }
 }
